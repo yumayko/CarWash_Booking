66a03d67f183bf438b5a8f24c8b557acbfad6739
Wahyu Nugroho Gultom <71364108+yumayko@users.noreply.github.com>
2025-09-19 23:44:01 +0700

    make migration
    make migration users, car_washes, services, and bookings tablee

---
+++ database/migrations/2025_09_19_163816_create_bookings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('car_wash_id')->constrained('car_washes')->onDelete('cascade');
+            $table->foreignId('service_id')->constrained('services')->onDelete('cascade');
+            $table->dateTime('booking_time');
+            $table->decimal('total_price', 10, 0);
+            $table->enum('status', ['pending', 'confirmed', 'completed', 'cancelled'])->default('pending');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
